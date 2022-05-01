82db97b91b80b56d82b5ed70e55169b73c4e2506
NikitaJamadar <nikhitajamadar1998@gmail.com>
2022-05-01 15:50:44 +0530

    [Nikita added AddUser in fundooNote project]

--- FundooNotes/Startup.cs
+++ FundooNotes/Startup.cs
@@ -1,14 +1,24 @@
+using BusinessLayer.Interfaces;
+using BusinessLayer.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using RepositoryLayer.FundooNoteContext;
+using RepositoryLayer.Interfaces;
+using RepositoryLayer.Services;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace FundooNotes
@@ -25,12 +35,67 @@ namespace FundooNotes
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers();
+            services.AddControllers().AddNewtonsoftJson(options => options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore);
+            services.AddTransient<IUserBL, UserBL>();
+            services.AddTransient<IUserRL, UserRL>();
+            services.AddDbContext<FunduContext>(opts => opts.UseSqlServer(Configuration["ConnectionStrings:Strings"]));
+
+            services.AddAuthentication(x =>
+            {
+                x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(x =>
+            {
+                x.RequireHttpsMetadata = false;
+                x.SaveToken = true;
+                x.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes("THIS_IS_MY_KEY_TO_GENERATE_TOKEN")),
+                    ValidateIssuer = false,
+                    ValidateAudience = false
+
+                };
+            });
+            services.AddSwaggerGen(setup =>
+            {
+                // Include 'SecurityScheme' to use JWT Authentication
+                var jwtSecurityScheme = new OpenApiSecurityScheme
+                {
+                    Scheme = "bearer",
+                    BearerFormat = "JWT",
+                    Name = "JWT Authentication",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.Http,
+                    Description = "Put *ONLY* your JWT Bearer token on textbox below!",
+
+                    Reference = new OpenApiReference
+                    {
+                        Id = JwtBearerDefaults.AuthenticationScheme,
+                        Type = ReferenceType.SecurityScheme
+                    }
+                };
+                setup.AddSecurityDefinition(jwtSecurityScheme.Reference.Id, jwtSecurityScheme);
+
+                setup.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    { jwtSecurityScheme, Array.Empty<string>() }
+                });
+
+            });
         }
+
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseAuthentication();
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+            });
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
@@ -49,3 +114,6 @@ namespace FundooNotes
         }
     }
 }
+
+
+       
---
+++ RepositoryLayer/Services/UserRL.cs
@@ -0,0 +1,68 @@
+﻿using DatabaseLayer;
+using Microsoft.Extensions.Configuration;
+using RepositoryLayer.Entity;
+using RepositoryLayer.FundooNoteContext;
+using RepositoryLayer.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RepositoryLayer.Services
+{
+    public class UserRL:IUserRL
+    {
+        FunduContext fundo;
+        public IConfiguration Configuration { get; }
+
+        //Creating constructor for initialization
+        public UserRL(FunduContext fundo, IConfiguration configuration)
+        {
+            this.fundo = fundo;
+            this.Configuration = configuration;
+        }
+
+        //Creating method to add user in database
+        public User AddUser(UserPostModel user)
+        {
+            try
+            {
+                User user1 = new User();
+                user1.userID = new User().userID;
+                user1.firstName = user.firstName;
+                user1.lastName = user.lastName;
+                user1.email = user.email;
+                user1.password = EncryptPassword(user.password);
+                user1.registerdDate = DateTime.Now;
+                user1.address = user.address;
+                fundo.Users.Add(user1);
+                fundo.SaveChanges();
+                return user1;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+        //Creating method to encrypt the password which is entered by user
+        public static string EncryptPassword(string password)
+        {
+            try
+            {
+                if (string.IsNullOrEmpty(password))
+                {
+                    return null;
+                }
+                else
+                {
+                    byte[] b = Encoding.ASCII.GetBytes(password);
+                    string encrypted = Convert.ToBase64String(b);
+                    return encrypted;
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
